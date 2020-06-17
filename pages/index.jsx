8ef090f671ed2c1a553c4ba501aff2935e8b8c55
Kormir38 <clement.alexandre@protonmail.com>
2020-06-17 08:27:04 +0200

    Initial page setup.

---
+++ pages/index.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Link from "next/link";
+
+export default function Home() {
+    return (
+        <main className="container is-fluid v-centered">
+            <section className="section animate__animated animate__zoomIn hero has-text-centered">
+                <figure className="image is-128x128 centered">
+                    <img src="/public/img/logo.png" alt="logo"/>
+                </figure>
+                <div className="hero-body">
+                    <div className="container">
+                        <h1 className="title">
+                            Hi! Welcome to my portfolio.
+                        </h1>
+                        <h2 className="subtitle">
+                            I'm Clément, a web and mobile developer.
+                        </h2>
+                    </div>
+                </div>
+                <div>
+                    <Link href="/about">
+                        <a className="button is-primary is-outlined">
+                            Learn more
+                        </a>
+                    </Link>
+                </div>
+            </section>
+        </main>
+    )
+}
