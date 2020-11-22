856588732c9e04206958f95361eb594d7842297c
Kormir38 <clement.alexandre@protonmail.com>
2020-11-22 11:08:35 +0100

    Added banner

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,25 +3,22 @@ import Link from "next/link";
 
 export default function Home() {
     return (
-        <main className="container is-fluid v-centered">
+        <main style="background-image:url('ToP-banner.jpg')" className="container is-fluid v-centered">
             <section className="section animate__animated animate__zoomIn hero has-text-centered">
-                <figure className="image is-128x128 centered">
-                    <img src="img/ToP-logo.jpg" alt="logo"/>
-                </figure>
                 <div className="hero-body">
                     <div className="container">
                         <h1 className="title">
                             Bienvenue sur l'index Tales of Pi
                         </h1>
                         <h2 className="subtitle">
-                            Petit projet réalisé pour s'entraîner avec Next.js
+                            Projet non affilié avec la chaîne Tales of Pi
                         </h2>
                     </div>
                 </div>
                 <div>
                     <Link href="/aboutProject">
                         <a className="button is-primary is-outlined">
-                            Le projet
+                            A propos du projet
                         </a>
                     </Link>
                 </div>
